0b2ad656f2e87105beca29d277fbd42ea187976e
Greg Benson <benson@usfca.edu>
2019-09-26 04:02:19 +0100

    week06 examples

--- week06/max_main.c
+++ week06/max_main.c
@@ -9,9 +9,18 @@ int main(int argc, char **argv)
 
     r = max_c(30,50);
     printf("max_c(30,50) = %d\n", r);
-
     r = max_a(30,50);
     printf("max_a(30,50) = %d\n", r);
+    
+    r = max_c(99,1);
+    printf("max_c(99,1) = %d\n", r);
+    r = max_a(99,1);
+    printf("max_a(99,1) = %d\n", r);
+
+    r = max_c(0,-1);
+    printf("max_c(0,-1) = %d\n", r);
+    r = max_a(0,-1);
+    printf("max_a(0,-1) = %d\n", r);    
 
     return 0;
 }
