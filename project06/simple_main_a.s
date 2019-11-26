471bab7731fc5fd55f255a54892440f2a4a2034a
Greg Benson <benson@usfca.edu>
2019-11-26 16:46:11 +0000

    Added simple_main_a.s for sub and cmp.

---
+++ project06/simple_main_a.s
@@ -0,0 +1,17 @@
+main:
+    bl simple
+end:
+    b end
+
+simple:
+    mov r0, #1
+    mov r1, #2
+    add r2, r0, r1
+    sub r3, r1, r0
+    sub r4, r0, r1
+    cmp r0, r0
+    cmp r0, r1
+    cmp r1, r2
+    mov r0, r2
+    bx lr
+
